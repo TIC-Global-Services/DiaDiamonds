6ebdbf7c2d2eac5b20d01f2c84418ed3d98203d9
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-02-17 18:54:27 +0530

    set content text centered and mobile responsiveness

--- components/Home/Arrivals.tsx
+++ components/Home/Arrivals.tsx
@@ -3,14 +3,16 @@ export default function Arrivals() {
   const secondImage = '/assets/img/Arrivals/ArrivalsImg2.png'; 
 
   return (
-    <div className="w-full h-dvh flex flex-wrap md:flex-nowrap">
+    <div className="w-full h-screen flex flex-wrap md:flex-nowrap">
       
-      <div className="w-full md:w-1/2 h-full">
+      <div className="relative w-full md:w-1/2 h-1/2 md:h-full">
         <img src={firstImage} alt="First Arrival" className="w-full h-full object-cover" />
+        <h2 className="absolute inset-0 font-medium text-[40px] leading-[100%] text-[#FFFFFF] w-full h-full flex justify-center items-center uppercase">NEW ARRIVALS</h2>
       </div>
       
-      <div className="w-1/2 h-full">
-        <img src={secondImage} alt="Second Arrival" className="w-full h-full object-cover" />
+      <div className="relative w-full md:w-1/2 h-1/2 md:h-full">
+        <img src={secondImage} alt="Second Arrival" className=" w-full h-full object-cover" />
+        <h2 className="absolute inset-0 font-medium text-[40px] leading-[100%] text-[#FFFFFF] w-full h-full flex justify-center items-center uppercase ">NEW COLLECTIONS</h2>
       </div>
 
     </div>
