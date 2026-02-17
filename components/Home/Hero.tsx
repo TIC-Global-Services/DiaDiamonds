f94a7075b1c578709577893a05d34375c0f7518d
Manoj Kumar <manojkumararumainathan@gmail.com>
2026-02-17 11:17:42 +0530

    Boilerplate Init - Dia

---
+++ components/Home/Hero.tsx
@@ -0,0 +1,14 @@
+import ContainerLayout from '@/layout/ContainerLayout'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <ContainerLayout>
+        <div>
+            <h1>Use this container layout inside the components and adjust.</h1>
+        </div>
+    </ContainerLayout>
+  )
+}
+
+export default Hero
