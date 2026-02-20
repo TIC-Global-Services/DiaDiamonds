4643e01fd45f6613ea94052c80e42086d5048d85
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-02-20 11:02:54 +0530

    set alignment for mobile screens

--- components/Home/Search.tsx
+++ components/Home/Search.tsx
@@ -16,13 +16,13 @@ export default function Search({isSearch, setIsSearch}:SearchPanelProps) {
     const searchImage2 = '/assets/img/Search/SearchImg2.jpg';
 
   return (
-    <div className={`absolute inset-0 w-full h-[100dvh] px-10 py-[26px] bg-[#ffffff] z-50 ${isSearch ? 'block' : 'hidden'}`} >
+    <div className={`absolute inset-0 w-full h-[100dvh] px-6 md:px-10 py-[26px] bg-[#ffffff] z-50 ${isSearch ? 'block' : 'hidden'}`} >
       <button className="pb-[30px] hover:cursor-pointer" onClick={()=>{setIsSearch(false)}}>
         <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.75 0C6.975 0 0 6.975 0 15.75C0 24.525 6.975 31.5 15.75 31.5C24.525 31.5 31.5 24.525 31.5 15.75C31.5 6.975 24.525 0 15.75 0ZM21.825 23.625L15.75 17.55L9.675 23.625L7.875 21.825L13.95 15.75L7.875 9.675L9.675 7.875L15.75 13.95L21.825 7.875L23.625 9.675L17.55 15.75L23.625 21.825L21.825 23.625Z" fill="black" fill-opacity="0.2"/></svg>
 
       </button>
       
-      <div className="pl-[70px]">
+      <div className="md:pl-[70px]">
         <h2 className="text-[24px] leading-[100%] tracking-[0%] text-[#000000] pb-[11px]">
           Search
         </h2>
