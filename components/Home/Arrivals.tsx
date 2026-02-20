ccf3c5bf819a31a4321a4ae1b34701615565fc63
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-02-20 16:14:53 +0530

    set scaling effect to two divs

--- components/Home/Arrivals.tsx
+++ components/Home/Arrivals.tsx
@@ -3,16 +3,20 @@ export default function Arrivals() {
   const secondImage = '/assets/img/Arrivals/ArrivalsImg2.png'; 
 
   return (
-    <div className="w-full h-screen flex flex-wrap md:flex-nowrap">
+    <div className="w-full h-screen flex flex-wrap md:flex-nowrap overflow-hidden">
       
-      <div className="relative w-full md:w-1/2 h-1/2 md:h-full">
-        <img src={firstImage} alt="First Arrival" className="w-full h-full object-cover" />
+      <div className="overflow-hidden w-full md:w-1/2 h-1/2 md:h-full">
+      <div className="relative w-full h-full hover:scale-110 delay-100 duration-300 ease-in-out">
+        <img src={firstImage} alt="First Arrival" className="w-full h-full object-cover " />
         <h2 className="absolute inset-0 font-medium text-[40px] leading-[100%] text-[#FFFFFF] w-full h-full flex justify-center items-center uppercase">NEW ARRIVALS</h2>
       </div>
-      
-      <div className="relative w-full md:w-1/2 h-1/2 md:h-full">
-        <img src={secondImage} alt="Second Arrival" className=" w-full h-full object-cover" />
+      </div>
+
+      <div className="overflow-hidden w-full md:w-1/2 h-1/2 md:h-full">
+      <div className="w-full h-full relative hover:scale-110 delay-100 duration-300 ease-in-out ">
+        <img src={secondImage} alt="Second Arrival" className=" w-full h-full object-cover " />
         <h2 className="absolute inset-0 font-medium text-[40px] leading-[100%] text-[#FFFFFF] w-full h-full flex justify-center items-center uppercase ">NEW COLLECTIONS</h2>
+      </div>
       </div>
 
     </div>
