8e3aab214e7d30ec49def6592bbfc058b6a74e0a
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-02-21 18:48:35 +0530

    set viewport once to false

--- components/Home/Arrivals.tsx
+++ components/Home/Arrivals.tsx
@@ -5,7 +5,7 @@ import { useRef } from "react";
 export default function Arrivals() {
 
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const isInView = useInView(containerRef, {amount:0.2, once:true});
+  const isInView = useInView(containerRef, {amount:0.2, once:false});
   const firstImage = '/assets/img/Arrivals/ArrivalsImg1.png'; 
   const secondImage = '/assets/img/Arrivals/ArrivalsImg2.png'; 
 
@@ -16,6 +16,7 @@ export default function Arrivals() {
       initial={{height:'10%'}}
        animate={isInView ? {height:'100%'}: {height:'10%'}}
        transition={{duration:1.2, ease:"easeInOut", delay:0.3}}
+       viewport={{once:false}}
        
         className="overflow-hidden w-full md:w-1/2 h-1/2 md:h-[100px] ">
       <div className="relative w-full h-full hover:scale-110 delay-100 duration-300 ease-in-out">
@@ -28,7 +29,7 @@ export default function Arrivals() {
       initial={{height:'10%'}}
        animate={isInView ? {height:'100%'}: {height:'10%'}}
        transition={{duration:1.2, ease:"easeInOut"}}
-       
+       viewport={{once:false}}
 
       className="absolute bottom-0 right-0 overflow-hidden w-full md:w-1/2 h-1/2 md:h-[100px]">
       <div className="w-full h-full relative hover:scale-110 delay-100 duration-300 ease-in-out ">
