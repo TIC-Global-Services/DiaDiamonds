859948d78b03fe15907455e81696260fb9d09ce9
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-02-23 13:46:52 +0530

    set image traslate to negative y to display the subject

--- components/About/Value.tsx
+++ components/About/Value.tsx
@@ -2,18 +2,18 @@ export default function Value() {
 
     const BgImage = '/assets/img/About/Value/BgImage.png';
   return (
-    <div className="w-full h-[100vh] relative ">
+    <div className="w-full h-[100svh]  relative overflow-hidden">
       {/* Background image */}
       <img
         src={BgImage}
         alt="Value Background"
-        className="w-full object-cover"
+        className="object-contain -translate-y-[20%] "
       />
 
       {/* Overlay content */}
       <div className="absolute inset-0 w-full flex justify-center items-center">
         <p className="md:w-[70%] text-[24px] leading-[100%] text-center text-[#FFFFFF]">
-          “Designed to capture light, crafted to capture hearts — Dia Diamonds brings brilliance to every chapter of your story.”
+          Designed to capture light, crafted to capture hearts — Dia Diamonds brings brilliance to every chapter of your story.
         </p>
       </div>
     </div>
