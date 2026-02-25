1bb96e467abe59bb1233da0102bf467f259f8dc7
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-02-26 00:33:14 +0530

    set responsiveness

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ContainerLayout from "@/layout/ContainerLayout";
+
 
 
 const Hero = () => {
@@ -10,7 +10,7 @@ const Hero = () => {
   return (
    
 
-        <div className="w-full h-screen relative overflow-scroll pointer-events-" style={{scrollbarWidth:'none'}}>
+        <div className="w-full h-screen relative overflow-scroll " style={{scrollbarWidth:'none'}}>
 
           
             <img 
@@ -23,18 +23,22 @@ const Hero = () => {
             
             <div className="absolute w-full h-full bottom-0 left-0 ">
 
-                <div className="w-full h-full flex flex-col gap-6 justify-center items-center  md:items-start md:justify-end px-6 sm:px-[34px] md:px-10 lg:px-[56px] xl:px-[64px] 2xl:px-20 py-8 md:py-22">
+                <div className="w-full h-full flex flex-col gap-[14px] justify-center items-center  md:items-start md:justify-end px-6 sm:px-[34px] md:px-10 lg:px-[56px] xl:px-[64px] 2xl:px-20 py-8 md:py-22">
 
                   <h1 className="text-center md:text-left font-medium text-[44px] leading-[46px] uppercase pb-[14px] text-[#FFFFFF]">
                   Crafted to last forever
                 </h1>
 
-                <p className="w-full text-center md:w-[70%] md:text-left text-[22px] leading-[24px] text-[#FFFFFF] pb-[24px]">
+                <p className="w-full text-center md:w-[40%] md:text-left text-[22px] leading-[24px] text-[#FFFFFF] pb-[12px] " >
                   Iconic Brand Feel Designed to Shine Today And Endure Forever
                 </p>
 
-                <button className="BtnAnimation cursor-pointer w-[213px] h-[44px] border border-[1px] tracking-[0.166rem] backdrop-blur-md border-[#FFFFFF]/.1 text-[#e7dfd7] bg-[#87888A]/20 text-[10px] md:text-base tracking-[6px] md:tracking-[3px]  md:bg-[#000000]/10 rounded-full  hover:scale-110 active:scale-100">
-                    DISCOVER MORE
+                <button className="
+                BtnAnimation cursor-pointer w-[213px] h-[44px]
+                border border-[1px] tracking-[0.166rem backdrop-blur-[4px] border-[#FFFFFF]/20 shadow-[0_4_4px_0_#000000]
+                text-[#e7dfd7] text-[10px] md:text-base tracking-[6px] md:tracking-[3px] 
+                bg-transparent rounded-full  hover:scale-110 active:scale-100 ">
+                   <a href="https://www.chopard.com/en-in/jewellery-happy-hearts">DISCOVER MORE</a> 
                 </button>
 
                 </div>
