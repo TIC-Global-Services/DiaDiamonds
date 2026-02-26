303dfa779c7d5cb8915ae83f6d0e53a8f4891df2
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-02-26 14:18:14 +0530

    set animation & responsiveness

--- components/About/Hero.tsx
+++ components/About/Hero.tsx
@@ -1,47 +1,76 @@
+"use client";
+
 import ContainerLayout from "@/layout/ContainerLayout";
+import { motion } from "framer-motion";
 
 export default function Hero() {
 
-    const ImageLeft = '/assets/img/About/Hero/HeroImgLeft.png';
-    const ImageRight = '/assets/img/About/Hero/HeroImgRight.png';
+  const ImageLeft = '/assets/img/About/Hero/HeroImgLeft.png';
+  const ImageRight = '/assets/img/About/Hero/HeroImgRight.png';
 
   return (
     <ContainerLayout>
       {/* Outer flex container */}
-      <div className="w-full flex flex-wrap md:flex-nowrap justify-center items-center h-[100vh]">
-        {/* First inner div */}
-        <div className="flex flex-col justify-center items-center mx-auto w-full md:w-2/5 gap-6">
-          {/* Heading */}
-          <h2 className="text-[40px] font-medium leading-[100%] text-[#000000] uppercase">
+      <div className="w-full flex flex-wrap md:flex-nowrap justify-center items-center md:h-[100vh] pt-[35%] md:pt-0 pb-[24%] md:pb-0">
+        
+        {/* Text content */}
+        <div className="flex flex-col justify-center items-center mx-auto w-full md:w-2/5 gap-6 order-2 md:order-1 pt-[20%]">
+          
+          <motion.h2 
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay:0.2, ease:"easeInOut" }}
+            viewport={{once:false}}
+            className="md:text-[40px] font-medium md:leading-[100%] text-[#000000] uppercase"
+          >
             ABOUT US
-          </h2>
+          </motion.h2>
 
-          {/* Paragraph */}
-          <p className="text-[20px] leading-[140%] text-[#000000] text-center md:text-left">
+          <motion.p 
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay:0.4, ease:"easeInOut" }}
+            viewport={{once:false}}
+            className="md:text-[20px] md:leading-[140%] text-[#000000] text-left md:text-left"
+          >
             Dia Diamonds is the modern expression of a legacy built on trust, integrity, and exceptional craftsmanship. We create timeless diamond jewellery that blends heritage values with contemporary design, ensuring every piece reflects elegance, authenticity, and superior quality. With a strong commitment to ethical sourcing and meticulous attention to detail.
-          </p>
+          </motion.p>
         </div>
 
-        {/* Second inner div */}
-        <div className="w-full md:w-3/5 h-full flex items-end relative gap-4">
-          {/* First image wrapper */}
-          <div className="w-[35%] md:shrink-0 md:grow-0">
+        {/* Image content */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay:0.6, ease:"easeInOut" }}
+          viewport={{once:false}}
+          className="w-full md:w-3/5 h-full flex items-start md:items-end relative md:gap-4 order-1 md:order-2"
+        >
+
+          {/* Left image wrapper (mobile overlap) */}
+          <motion.div
+            className="h-full md:h-auto mt-auto w-2/5 md:w-[35%] shrink-0 md:grow-0 relative -right-8 md:right-0"
+            initial={{ x: 20 }}
+            whileInView={{ x: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            viewport={{ once: true }}
+          >
             <img
               src={ImageLeft}
               alt="Hero Image 1"
-              className="w-full aspect-square -translate-y-[10%] translate-x-[50%]  z-20  "
+              className="w-full aspect-square -translate-y-[10%] md:translate-x-[50%] md:scale-100 scale-110 z-20"
             />
-          </div>
+          </motion.div>
 
-          {/* Second image wrapper */}
-          <div className="overflow-hidden w-[60%] h-[95%] md:shrink md:grow-0">
+          {/* Right image wrapper */}
+          <div className="overflow-hidden w-3/5 md:w-[60%] md:h-[95%] md:shrink md:grow-0">
             <img
               src={ImageRight}
               alt="Hero Image 2"
-              className="w-full aspect-[422/633]  rounded-t-full z-10 "
+              className="w-full md:aspect-[422/633] aspect-[271/406] rounded-t-full z-10"
             />
           </div>
-        </div>
+        </motion.div>
+
       </div>
     </ContainerLayout>
   );
