4d15998740bbd3e9da744abf0b20455664500215
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-02-26 10:48:45 +0530

    set responsiveness

--- components/Home/Q&A.tsx
+++ components/Home/Q&A.tsx
@@ -4,29 +4,26 @@ import QA from "./Reusable/QA";
 import { Parallax } from "react-scroll-parallax";
 
 export default function QandA() {
-
-  const QAImageLeft = '/assets/img/Q&A/QAImage.png';
-  
+  const QAImageLeft = "/assets/img/Q&A/QAImage.png";
 
   return (
     <div className="w-full relative">
 
       {/* Non-absolute content container */}
-      <div className="relative z-20 w-full py-20 px-10 flex justify-end items-start gap-[60px] flex-wrap md:flex-nowrap">
+      <div className="relative z-20 w-full py-20 px-10 flex justify-end items-center gap-[60px] flex-wrap md:flex-nowrap">
 
-        <div className="w-[30%] overflow-hidden delay-100 duration-300 ease-in-out">
+        <div className="w-[40%] mx-auto md:mx-0 overflow-hidden delay-100 duration-300 ease-in-out">
           <Parallax speed={-10}>
-          <img
-          src={QAImageLeft}
-          alt="Q&A visual"
-          className="w-full aspect-[448/673] object-contain hover:scale-110"
-        />
-        </Parallax>
+            <img
+              src={QAImageLeft}
+              alt="Q&A visual"
+              className="w-full aspect-[157/234] scale-110 md:scale-100 md:aspect-[448/673] object-contain hover:scale-110"
+            />
+          </Parallax>
         </div>
-        
 
         <div className="flex flex-col justify-center items-center w-full md:w-[55%]">
-          <h2 className="font-medium text-[40px] leading-[100%] text-[#000000] uppercase pb-10">
+          <h2 className="font-medium md:text-[40px] leading-[100%] text-[#000000] uppercase pb-10 mx-auto md:mx-0">
             Popular Questions
           </h2>
 
@@ -39,21 +36,13 @@ export default function QandA() {
           <QA question="What are Solitaire Rings?" answer="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
           <QA question="What is your return or exchange policy?" answer="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
         </div>
-
-        
       </div>
 
       {/* Absolute background container */}
-      <div className="absolute inset-0 w-full h-full flex z-10">
-        
-        <div className="bg-[#EAE3D9] w-[42%]">
-        </div>
-
-        <div className="bg-[#F9F9F9] w-full">
-        </div>
-
+      <div className="absolute inset-0 w-full h-full flex z-10 flex-col md:flex-row">
+        <div className="bg-[#EAE3D9] md:w-[42%] w-full aspect-[412/240] md:aspect-auto"></div>
+        <div className="bg-[#F9F9F9] w-full"></div>
       </div>
-
     </div>
   );
 }
--- components/Home/Reusable/QA.tsx
+++ components/Home/Reusable/QA.tsx
@@ -3,36 +3,49 @@
 import { useState } from "react";
 
 type QAProps = {
-    question:string,
-    answer:string,
-}
+  question: string;
+  answer: string;
+};
 
-export default function QA({ question, answer }:QAProps) {
-
-    const [isAnswer, setIsAnswer] = useState<boolean>(false);
+export default function QA({ question, answer }: QAProps) {
+  const [isAnswer, setIsAnswer] = useState<boolean>(false);
 
   return (
     <div className="w-full pt-[30px]">
       
       {/* Question Row */}
-      <div className="w-[90%] flex justify-between flex-nowrap">
-        <p className="text-[20px] leading-[100%] text-[#000000] md:shrink-0 pl-4 hover:cursor-pointer" onClick={()=>setIsAnswer(!isAnswer)} >
+      <div className="w-full md:w-[90%] flex justify-between items-center flex-nowrap">
+        
+        <p
+          className="text-[14px] md:text-[20px] leading-none text-[#000000] shrink md:shrink-0 pl-4 cursor-pointer"
+          onClick={() => setIsAnswer(!isAnswer)}
+        >
           {question}
         </p>
 
-        <button onClick={()=>setIsAnswer(!isAnswer)} className={`${isAnswer ? 'rotate-180' : ''} hover:cursor-pointer`}>
-            <svg width="10" height="5" viewBox="0 0 10 5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 0L5 5L10 0H0Z" fill="black"/></svg>
+        <button
+          onClick={() => setIsAnswer(!isAnswer)}
+          className={`${isAnswer ? "rotate-180" : ""} flex items-center justify-center transition-transform duration-200`}
+        >
+          <svg
+            className="block w-[10px] h-[5px]"
+            viewBox="0 0 10 5"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0L5 5L10 0H0Z" fill="black" />
+          </svg>
         </button>
       </div>
 
       {/* Answer Section */}
-      <div className={`${isAnswer ? 'block' : 'hidden' } w-full  pt-4 `}>
-        <p className="text-[16px] leading-[150%] text-[#000000] pl-4">
+      <div className={`${isAnswer ? "block" : "hidden"} w-full pt-4`}>
+        <p className="text-[12px] md:text-[16px] leading-[150%] text-[#000000] pl-4">
           {answer}
         </p>
       </div>
-      <div className="w-full border-b border-[#000000]/50 pt-[30px]"></div>
 
+      <div className="w-full border-b border-[#000000]/50 pt-[30px]"></div>
     </div>
   );
 }
