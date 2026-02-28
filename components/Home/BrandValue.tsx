1b9f0e7049058abe9c57a81a5cf2281cd0438c7f
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-02-28 07:22:34 +0530

    recreate  BrandValue

--- components/Home/BrandValue.tsx
+++ components/Home/BrandValue.tsx
@@ -10,13 +10,13 @@ export default function BrandValue() {
 
 
   return(
-    <div className="relative w-full h-auto ">
-      <img src={BrandValueImg} alt="Rings Image" className=" w-full h-auto " />
+    <div className="relative w-full h-[100vh] md:h-auto ">
+      <img src={BrandValueImg} alt="Rings Image" className=" w-full h-[100vh] object-cover md:object-auto md:h-auto " />
 
-      <div className="w-full md:w-[48%] absolute bottom-0 right-0 md:mb-[16.25%]  flex flex-col justify-center md:justify-normal items-start">
+      <div className="w-[59%] md:w-[48%] h-auto absolute bottom-0 right-0 mb-[55.34%] md:mb-[16.25%] mr-[1.18%] md:mr-0 flex flex-col justify-center md:justify-normal items-start">
         <h2>Thoughtfully curated for you </h2>
-        <p>Your diamond journey starts here. Where brilliance meets craftsmanship, and every sparkle tells your story.</p>
-        <button>LEARN MORE</button>
+        <p className="leading-[100%] md:leading-[140%]">Your diamond journey starts here. Where brilliance meets craftsmanship, and every sparkle tells your story.</p>
+        <button className="BtnAnimation bg-[#FFFFFF]/50 rounded-full px-[4.49%] py-[2.18%] md:px-[2.08%] md:py-[0.83%] font-medium text-[12px] backdrop-blur-lg">LEARN MORE</button>
       </div>
     </div>
   )
