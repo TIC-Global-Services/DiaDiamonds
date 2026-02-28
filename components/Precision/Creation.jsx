9038375044575017df419439689677e933e99953
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-02-28 10:14:18 +0530

    set new Creation

--- components/Precision/Creation.jsx
+++ components/Precision/Creation.jsx
@@ -3,143 +3,134 @@
 import ContainerLayout from "@/layout/ContainerLayout";
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { span } from "framer-motion/client";
 
 export default function Creation() {
+
+  const [currentCard, setCurrentCard] = useState(1);
+
   const contentJSON = [
     {
-      imageLeft: "/assets/img/Precision/Creation/bgImage.jpg",
+      imageLeft: "/assets/img/Precision/Creation/bgImage1.jpg",
       overlayText:'The Diamond Seed',
       contentText: [
-        { text: "Every lab grown diamond used at " },
-        { text: "Dia Diamonds", bold: true },
-        { text: " begins with a microscopic slice of real diamond called a seed. This seed acts as the foundation where pure carbon atoms attach and grow." }
-      ]
-    },
-    {
-      imageLeft: "/assets/img/Precision/Creation/bgImage1.jpg",
-      overlayText:'Growth Proccess',
-      contentText: [
-        { text: "" },
-        { text: "Lab Grown Diamonds", bold: true },
-        { text: " are created using High Pressure High Temperature (HPHT) or Chemical Vapor Deposition (CVD) where carbon atoms crystallize under controlled conditions." }
+        { text: "Every lab grown diamond used at ", bold:false, newline:false },
+        { text: "Dia Diamonds", bold: true, newline:false },
+        { text: " begins with a microscopic slice of real diamond called a seed. This seed acts as the foundation where pure carbon atoms attach and grow." , bold:false, newline:false},
+        { text: "It is carefully placed inside a controlled growth chamber designed to replicate natural diamond-forming conditions.", newline:true}
       ]
     },
     {
       imageLeft: "/assets/img/Precision/Creation/bgImage2.jpg",
+      overlayText:'Growth Proccess',
+      contentText: [
+        { text: "Lab Grown Diamonds", bold: true, newline:false },
+        { text: " are grown using advanced scientific methods that recreate nature with precision.", bold:false, newline:false },
+        { text: "Through either High Pressure High Temperature (HPHT) or Chemical Vapor Deposition (CVD), carbon atoms crystallize around the seed under extreme heat and controlled energy.", newline:true, bold:false}
+      ]
+    },
+    {
+      imageLeft: "/assets/img/Precision/Creation/bgImage3.jpg",
       overlayText:'Final Creation',
       contentText: [
-        { text: "Over several weeks, the diamond grows into a rough crystal, monitored for clarity and structure before being cut and polished into brilliance." }
+        { text: "Over several weeks, the diamond grows steadily into a rough crystal. Every stage is monitored to ensure clarity, color balance, and structural perfection.", bold:false, newline:false },
+        { text: "At this stage, the diamond is complete in composition and real gemstone in its raw form, ready for refinement.", bold:false, newline:true}
       ]
     }
   ];
 
-  const [currentContent, setCurrentContent] = useState(0);
-
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentContent((prev) => (prev + 1) % contentJSON.length);
-    }, 6000);
+      setCurrentCard((prev) => (prev + 1) % contentJSON.length);
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
   const handleNext = () => {
-    setCurrentContent((prev) => (prev + 1) % contentJSON.length);
+    setCurrentCard((prev) => (prev + 1) % contentJSON.length);
   };
 
   const handleSelect = (index) => {
-    setCurrentContent(index);
+    setCurrentCard(index);
   };
 
-  if (!contentJSON[currentContent]) return null;
+  if (!contentJSON[currentCard]) return null;
 
   return (
     <div className="relative w-full h-auto bg-white overflow-hidden">
       
       {/* Background Shapes */}
-      <div className="absolute top-0 right-[-20%] w-[70%] h-full bg-[#7C3C3C]/10 rounded-full z-10" />
-      <div className="absolute top-0 right-[-30%] w-[70%] h-full bg-[#F2F2F2] md:bg-[#ccc9c9] rounded-full z-0" />
+      <div className="absolute top-[62.65%] left-[69.17%] md:left-[59.38%] md:top-0  w-[62.62%] aspect-square md:aspect-none md:w-[70%] md:h-full bg-[#7C3C3C]/13 rounded-full z-10" />
+      <div className="absolute top-[64%] left-[81.93%] md:left-[75.14%] md:top-0 w-[62.62%] aspect-square md:aspect-none md:w-[70%] md:h-full bg-[#F2F2F2]  rounded-full z-0" />
 
-      <div className="relative z-20 w-full h-full flex flex-col">
+
+       
+      <div className="relative z-20 w-full h-full flex flex-col py-[25%] md:py-[5.83%]">
 
         {/* Title */}
-        <div className="pt-[5%] text-center shrink-0 md:w-[40%] mx-auto">
-          <h2>
-            designed for you <br />
+        <div className="text-center shrink-0 w-[96.71%] md:w-[40%] mx-auto pb-[5.49%]">
+          <h2 className="w-full">
+            designed for you
             cherished forever.
           </h2>
         </div>
 
-        {/* Card Section */}
-        <div className="w-full h-full flex-grow flex items-center justify-center px-4 py-[2%]">
-          <ContainerLayout>
-            <div className="relative flex flex-col md:flex-row items-center justify-between gap-8 md:gap-16">
-              
-              {/* Image */}
-              <AnimatePresence mode="wait" className='relative'>
-                <motion.img
-                  key={currentContent}
-                  src={contentJSON[currentContent].imageLeft}
-                  alt="Diamond Process"
-                  className="w-full md:w-[40%] aspect-[4/3] rounded-[20px] object-cover cursor-pointer"
-                  initial={{ opacity: 0, x: -40 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  exit={{ opacity: 0, x: 40 }}
-                  transition={{ duration: 1, ease: "easeInOut" }}
-                  onClick={handleNext}
-                />
-                <motion.p
-                 initial={{ opacity: 0, x: -40 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  exit={{ opacity: 0, x: 40 }}
-                  transition={{ duration: 1, ease: "easeInOut" }}
-                className="m-0 absolute bottom-4 left-4 text-[#FFFFFF] text-[16px]">{contentJSON[currentContent].overlayText}</motion.p>
-              </AnimatePresence>
+        {/* Content Div */}
 
-              {/* Text */}
-              <div className="relative w-full md:w-[45%] min-h-[150px] flex items-center">
-                <AnimatePresence mode="wait">
-                  <motion.div
-                    key={currentContent + "-text"}
-                    className="absolute w-full"
-                    initial={{ opacity: 0, x: 40 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: -40 }}
-                    transition={{ duration: 1, ease: "easeInOut" }}
-                  >
-                    <div className="border-l-[3px] border-l-[#5a5a5a] pl-6 md:pl-8">
-                      <p className="text-sm md:text-[24px] 2xl:text-[40px] leading-[140%]">
-                        {contentJSON[currentContent].contentText.map((part, index) => (
-                          <span
-                            key={index}
-                            className={part.bold ? "font-semibold" : ""}
-                          >
-                            {part.text}
-                          </span>
-                        ))}
-                      </p>
-                    </div>
-                  </motion.div>
-                </AnimatePresence>
-              </div>
+        <div className=" w-full flex justify-between flex-wrap md:flex-nowrap md:gap-[8.54%] items-center px-[5.21%] ">
 
-            </div>
-          </ContainerLayout>
+          {/* Item 1 */}
+          <AnimatePresence mode="wait">
+          <motion.div
+
+          key={currentCard}
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          exit={{opacity:0}}
+          transition={{duration:0.5, delay:0.2, ease:"easeIn"}}
+
+          className="relative w-full md:w-[41.67%] grow  aspect-600/450 border-0 rounded-[20px] overflow-hidden mb-[18.20%] bg-black md:mb-0"> 
+
+              <img src={contentJSON[currentCard].imageLeft} alt={contentJSON[0].overlayText} className="w-full h-auto object=cover object-center mt-[-25%]" />
+
+              <p className="absolute bottom-0 left-0 mb-[7.83%] ml-[9.5%] mr-0 mt-0 text-[#FFFFFF] font-normal text-[16px]">{contentJSON[0].overlayText}</p>
+          </motion.div>
+          </AnimatePresence>
+
+          {/* Item 2 */}
+
+          <AnimatePresence mode="wait">
+          <motion.div
+
+           key={currentCard}
+          initial={{opacity:0, x:20}}
+          animate={{opacity:1, x:0}}
+          exit={{opacity:0, x:20}}
+          transition={{duration:0.5, delay:0.2, ease:"easeIn"}}
+          
+          className="w-full md:w-[41.67%] border-l-[3px] border-0 border-l-[#5A5A5A] px-[7.77%] md:px-0 md:pl-[2.22%] ">
+  {contentJSON[currentCard].contentText.map((data, id) => {
+    if (data.bold) {
+      return <span key={id} className="font-semibold md:tracking-[5%] text-[#383838] text-[13px] md:text-[24px] leading-[100%] md:leading-[140%]">{data.text}</span>;
+    } else if (data.newline) {
+      return <p className=" text-[#383838] text-[13px] md:text-[24px] leading-[100%] md:leading-[140%] pt-[4%]" key={id}>{data.text}</p>;
+    } else {
+      return <span className=" text-[#383838] text-[13px] md:text-[24px] leading-[100%] md:leading-[140%]" key={id}>{data.text}</span>;
+    }
+  })}
+</motion.div>
+</AnimatePresence>
+
+
         </div>
 
-        {/* Progress Buttons */}
-        <div className="w-full flex justify-center items-center gap-3 pb-[10%]">
-          {contentJSON.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => handleSelect(index)}
-              className={`h-2 rounded-full transition-all duration-300 cursor-pointer ${
-                currentContent === index
-                  ? "w-8 bg-[#431a1a]"
-                  : "w-4 bg-black/20"
-              }`}
-            />
-          ))}
+        <div className="w-[14.08%] md:w-[4.03%] mx-auto flex justify-between items-center md:gap-[0.4%] pt-[12.86%] md:pt-[2.36%]">
+
+          <button onClick={()=>setCurrentCard(0)} className={` ${currentCard == 0 ? 'w-[37.93%] aspect-[22/6] bg-[#431A1A]' : 'w-[20.69%] aspect-[12/6] bg-[#000000]/10'}  rounded-[10px]  cursor-pointer `}></button>
+          <button onClick={()=>setCurrentCard(1)} className={` ${currentCard == 1 ? 'w-[37.93%] aspect-[22/6] bg-[#431A1A]' : 'w-[20.69%] aspect-[12/6] bg-[#000000]/10'}  rounded-[10px]  cursor-pointer `}></button>
+          <button onClick={()=>setCurrentCard(2)} className={` ${currentCard == 2 ? 'w-[37.93%] aspect-[22/6] bg-[#431A1A]' : 'w-[20.69%] aspect-[12/6] bg-[#000000]/10'}  rounded-[10px]  cursor-pointer `}></button>
+
         </div>
 
       </div>
