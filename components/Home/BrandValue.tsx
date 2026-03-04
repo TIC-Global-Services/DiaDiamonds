602c31b875076b8d6083f702da7424288110cf2d
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-03-04 17:41:23 +0530

    change color arg to textColor to match the argument and fix the typescript error

--- components/Home/BrandValue.tsx
+++ components/Home/BrandValue.tsx
@@ -17,7 +17,7 @@ export default function BrandValue() {
         <h2>Thoughtfully curated for you </h2>
         <p className="leading-[100%] md:leading-[140%]">Your diamond journey starts here. Where brilliance meets craftsmanship, and every sparkle tells your story.</p>
         {/* <button className="BtnAnimation bg-[#FFFFFF]/50 rounded-full px-[4.49%] py-[2.18%] md:px-[2.08%] md:py-[0.83%] font-medium text-[12px] backdrop-blur-lg">LEARN MORE</button> */}
-        <PrimaryBtn text="LEARN MORE" color="black" />
+        <PrimaryBtn text="LEARN MORE" textColor="black" />
       </div>
     </section>
   )
